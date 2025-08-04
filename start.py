eceea16676476dfa18f5a59b7c59ee08d812ffa2
sergii.kravchuk <si@gmail.com>
2025-08-03 20:12:49 -0400

    Initial changes with access to mongodb

---
+++ start.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+from datetime import datetime
+
+
+CONNECTION_STRING = "mongodb://root:root@localhost:27017"
+
+database_name = "user-account"
+collection_name = "users"
+
+now = datetime.now()
+
+document = {"user_id": 10, "user": "Sergii Kravchuk", "cur_time":now.strftime("%Y-%m-%d %H:%M:%S")}
+
+client = MongoClient(CONNECTION_STRING)
+
+dbloc = client[database_name]
+colloc = dbloc[collection_name]
+
+colloc.insert_one(document)
+
+all_documents_cursor = colloc.find({})
+print("All documents:")
+for doc in all_documents_cursor:
+    print(doc)
